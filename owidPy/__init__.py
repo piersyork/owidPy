47e864030c13913b27d890ef2dae7813ac3c52b2
piersyork <85755776+piersyork@users.noreply.github.com>
2021-10-23 16:19:09 +0100

    oop implementation

--- owidPy/__init__.py
+++ owidPy/__init__.py
@@ -5,4 +5,76 @@ Created on Sat Oct 23 14:43:26 2021
 
 @author: piers
 """
-from owidPy import owid, owid_search
+from search import owid_search
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from functools import reduce
+#from plotnine import ggplot, geom_line, aes, theme_538
+import webbrowser
+
+
+class Owid:
+    def __init__(self, chart_id):
+        self.chart_id = chart_id
+        self.url = self.get_data_url()
+        self.data = self.get_data()
+        
+    def get_data(self):
+        data_url = self.url
+        json_raw = requests.get(data_url).json()
+
+        vars = json_raw['variables']
+
+        datasets = []
+        for i in vars:
+            val_name = vars[i]['name']
+            data_dict = {'entity_id': vars[i]['entities'],
+                         'year': vars[i]['years'],
+                         val_name: vars[i]['values']}
+            #data = pd.DataFrame(data_dict).rename('')
+            datasets.append(pd.DataFrame(data_dict))
+    
+        all_data = reduce(lambda x, y: pd.merge(x, y, on = ['entity_id', 'year']), datasets)
+        
+        entity_ids = list(json_raw['entityKey'].keys())
+        codes = []
+        entities = []
+        
+        for id in entity_ids:
+            codes.append(json_raw['entityKey'][id]['code'])
+            entities.append(json_raw['entityKey'][id]['name'])
+    
+        entity_key_dict = {'entity_id': entity_ids,
+                           'entity': entities,
+                           'code': codes}
+    
+        entity_key = pd.DataFrame(entity_key_dict)
+        
+        all_data['entity_id'] = all_data['entity_id'].astype(str)
+        
+        data = all_data.merge(right = entity_key, how = 'left', on = 'entity_id')
+        
+        data = data[['entity', 'code'] + [c for c in data if c not in ['entity', 'code']]]
+        
+        data.pop('entity_id')
+    
+        return data
+        
+        
+    def get_data_url(self):
+        chart_id = self.chart_id
+        url = f"https://ourworldindata.org/grapher/{chart_id}"
+
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        preload_link = soup.find_all('link', attrs = {'rel':'preload'})[0].get('href')
+    
+        full_url = f"https://ourworldindata.org{preload_link}"
+    
+        return full_url
+    
+    def view_chart(self):
+        webbrowser.open(f'https://ourworldindata.org/grapher/{self.chart_id}')
+    
--- tests.py
+++ tests.py
@@ -11,12 +11,19 @@ os.chdir('/Users/piers/Documents/OWID/owidPy')
 from owidPy import owid, owid_search
 import seaborn as sns
 
+owid = Owid("legally-recognized-rights-to-land")
 
 
+owid.data
+
+
+owid = Owid("human-rights-scores")
+
+owid.view_chart()
 
 owidPy.owid_search('rights')
 
-owid_search("rights")
+owid_search("legally-recognized-rights-to-land")
 
 df = owid("legally-recognized-rights-to-land")
 
